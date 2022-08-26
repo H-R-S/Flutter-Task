ff308cdc761920e71265e779aeaf61235f146b58
H-R-S <369haris@gmail.com>
2022-08-26 17:00:24 +0500

    New screen added | Home Screen

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screens/login/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Task',
+        home: LoginScreen());
+  }
+}
