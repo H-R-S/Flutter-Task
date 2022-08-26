8c60cfc10cad8956b665766bb242f5aa9e5ad609
H-R-S <369haris@gmail.com>
2022-08-26 17:00:18 +0500

    New screen added | Home Screen

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:task/constants/style.dart';
+import 'package:task/screens/post/post_screen.dart';
+import 'package:task/widgets/app_bar/my_app_bar.dart';
+
+class HomeScreen extends StatelessWidget {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: primaryColor,
+            onPressed: () {},
+            child: const Icon(Icons.add, color: Colors.white)),
+        appBar: MyAppBar(
+            context,
+            scaffoldKey,
+            () {},
+            [
+              const Tab(text: "Posts"),
+              const Tab(text: "Profile"),
+            ],
+            "Home"),
+        body: TabBarView(children: [PostScreen(), Container()]),
+      ),
+    );
+  }
+}
