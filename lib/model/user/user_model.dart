1a7ce5824bbbf991e5bcb3202b934da44a26c4b7
H-R-S <369haris@gmail.com>
2022-08-27 23:33:13 +0500

    New model added | User Model

---
+++ lib/model/user/user_model.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel data) => json.encode(data.toJson());
+
+class UserModel {
+  int? id;
+  String? name;
+  String? username;
+  String? email;
+  Address? address;
+
+  UserModel({
+    this.id,
+    this.name,
+    this.username,
+    this.email,
+    this.address,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        id: json["id"],
+        name: json["name"],
+        username: json["username"],
+        email: json["email"],
+        address: Address.fromJson(json["address"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "username": username,
+        "email": email,
+        "address": address!.toJson(),
+      };
+}
+
+class Address {
+  Address({
+    this.street,
+    this.suite,
+    this.city,
+    this.zipcode,
+  });
+
+  String? street;
+  String? suite;
+  String? city;
+  String? zipcode;
+
+  factory Address.fromJson(Map<String, dynamic> json) => Address(
+        street: json["street"],
+        suite: json["suite"],
+        city: json["city"],
+        zipcode: json["zipcode"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "street": street,
+        "suite": suite,
+        "city": city,
+        "zipcode": zipcode,
+      };
+}
