60bf219b82552bb5785c49ec96330302265c90f4
H-R-S <369haris@gmail.com>
2022-08-27 23:34:08 +0500

    New widget added | My Text Field  Widget

--- lib/widgets/text_field/my_text_field.dart
+++ lib/widgets/text_field/my_text_field.dart
@@ -5,16 +5,28 @@ class MyTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hint;
   final bool isPassword;
+  final bool isVisible;
+  final Function()? onTap;
 
   const MyTextField(
-      {required this.controller, required this.hint, this.isPassword = false});
+      {required this.controller,
+      required this.hint,
+      this.isVisible = false,
+      this.isPassword = false,
+      this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      onTap: onTap,
+      obscureText: isVisible,
       cursorColor: Colors.grey,
       controller: controller,
       decoration: InputDecoration(
+          suffixIcon: isPassword
+              ? Icon(isVisible ? Icons.visibility_off : Icons.visibility,
+                  color: primaryColor)
+              : null,
           filled: true,
           fillColor: lightGrey,
           enabledBorder: OutlineInputBorder(
