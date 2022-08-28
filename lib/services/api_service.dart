f58ecde31c37fc6638efec7261435d29ff5dae1e
H-R-S <369haris@gmail.com>
2022-08-28 12:06:43 +0500

    Services updated | API Services

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -8,22 +8,21 @@ import '../model/post/post_model.dart';
 class APIService {
   static var client = http.Client();
 
-  static Future<UserModel?> getUser(id) async {
-    var url = Uri.parse("jsonplaceholder.typicode.com/users?id=$id");
+  static Future<List<UserModel>> getUser(id) async {
+    var url = Uri.parse("https://jsonplaceholder.typicode.com/users?id=$id");
 
     var response = await client.get(url);
 
     if (response.statusCode == 200) {
-      var data = jsonDecode(response.body);
+      var json = response.body;
 
-      return userModelFromJson(data);
+      return userModelFromJson(json);
     } else {
-      return null;
+      return [];
     }
   }
 
   static Future<List<PostModel>> getPosts() async {
-
     var postUrl = Uri.parse("https://jsonplaceholder.typicode.com/posts");
 
     var response = await client.get(postUrl);
