372bd8d1b29c5fe835ff83bfd92047aeec48644c
H-R-S <369haris@gmail.com>
2022-08-28 12:07:12 +0500

    Widget UI updated | Post Container Widget

--- lib/widgets/post_container/post_container.dart
+++ lib/widgets/post_container/post_container.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:task/constants/style.dart';
 
 class PostContainer extends StatelessWidget {
+  final num id;
+  final num userId;
   final String title;
   final String description;
 
-  const PostContainer({required this.title, required this.description});
+  const PostContainer(
+      {required this.id,
+      required this.userId,
+      required this.title,
+      required this.description});
 
   @override
   Widget build(BuildContext context) {
@@ -18,10 +24,14 @@ class PostContainer extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(title,
-                style:
-                    const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                style: TextStyle(
+                    color: id != userId ? null : primaryColor,
+                    fontSize: 16,
+                    fontWeight: id != userId ? null : FontWeight.bold)),
             const SizedBox(height: 5),
-            Text(description)
+            Text(description,
+                style: TextStyle(
+                    fontWeight: id != userId ? null : FontWeight.bold))
           ],
         ),
       ),
